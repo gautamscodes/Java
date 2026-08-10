dc518ef28c7fc5a1b0b63b782c73495527411773
gautamscodes <gautam07edu.py@gmail.com>
2026-08-11 01:18:18 +0530

    Variable2 is updated

---
+++ src/Core/Variable2.java
@@ -0,0 +1,13 @@
+package Core;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        String name = "Siddhant";
+        int age = 22;
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+
+}
