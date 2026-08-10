ec9b9ab85df136cd96cdc8e42a1a56ef965353bc
gautamscodes <gautam07edu.py@gamil.com>
2026-08-11 00:58:46 +0530

    Add/update .gitignore and remove ignored files from tracking

---
+++ src/Core/Variable1.java
@@ -0,0 +1,11 @@
+package Core;
+
+public class Variable1 {
+    public static void main(String[] args) {
+        int age = 10;
+        String name = "Gautam";
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+
+    }
+}
