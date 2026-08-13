eb66492c2f1a95bf97acdf075465ee2cb3616e63
gautamscodes <gautam07edu.py@gmail.com>
2026-08-13 10:32:06 +0530

    Array List 1

---
+++ src/Collections/List/ArrayList1.java
@@ -0,0 +1,19 @@
+package Collections.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1 {
+    public static void main(String[] args){
+        List<String> names = new ArrayList<>();
+
+        names.add("Gautam");
+        names.add("Soumya");
+        names.add("Alisha");
+        names.add("Aman");
+
+        System.out.println(names);
+
+
+    }
+}
